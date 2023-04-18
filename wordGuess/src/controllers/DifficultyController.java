87682f75e2c94c707423dd09a22b4c96d72b0a36
Carlos Rodriguez <CarlosR@10.0.0.131>
2023-04-18 11:29:21 -0400

    logo to click added, few changes in difficulty file. Game files started.

--- wordGuess/src/controllers/DifficultyController.java
+++ wordGuess/src/controllers/DifficultyController.java
@@ -1,8 +1,12 @@
 package controllers;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
@@ -10,6 +14,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class DifficultyController {
 
@@ -54,9 +59,17 @@ public class DifficultyController {
 		return this.difficulty;
 	}
 	
-	@FXML private void startGame() {
+	@FXML private void startGame(ActionEvent evt) {
 		if(this.easy.isSelected() || this.medium.isSelected() || this.hard.isSelected()) {
-			System.out.println("Difficulty: " + this.difficulty);
+			try {
+				AnchorPane root = FXMLLoader.load(getClass().getResource("/fxmlFiles/Game.fxml"));
+				Stage stage = (Stage) ((Node) evt.getSource()).getScene().getWindow();
+				Scene scene = new Scene(root);
+				stage.setScene(scene);
+				stage.centerOnScreen();
+				stage.show();
+			} catch (IOException e) {
+			}
 		}
 		else {
 			this.alert.show();
--- wordGuess/src/controllers/GameController.java
+++ wordGuess/src/controllers/GameController.java
@@ -1,5 +1,6 @@
 package controllers;
 
 public class GameController {
+	//button.setStyle("  -fx-border-style: none; -fx-border-width: 2px; -fx-border-insets: 0; -fx-font-size:4px; -fx-background-image: url('image.png')");
 
 }
