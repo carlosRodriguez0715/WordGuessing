0f1ae278514134836e7b2f38ad232d8407ac74b1
Carlos Rodriguez <CarlosR@10.0.0.131>
2023-04-19 11:38:05 -0400

    each label for letter and lines working. Label for tries added.

--- wordGuess/src/controllers/GameController.java
+++ wordGuess/src/controllers/GameController.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import java.util.Random;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -7,19 +9,39 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Font;
 
 public class GameController {
 	@FXML private AnchorPane bgPane, subPane, drawing;
 	@FXML private Button enterBtn;
 	@FXML private TextField input;
 	@FXML private TextArea hints;
-	@FXML private Label guessTitle;
+	@FXML private Label guessTitle, attemptsLabel;
 	@FXML private ListView<String> guessingList;
 	
 	@FXML private void initialize() {
 		this.enterBtn.setStyle("-fx-background-image: url('/other/logo2.png')");
+		generateWord();
 	}
 	
-	//button.setStyle("  -fx-border-style: none; -fx-border-width: 2px; -fx-border-insets: 0; -fx-font-size:4px; -fx-background-image: url('image.png')");
-
+	private void generateWord() {
+		Random rnd = new Random();
+		int r = rnd.nextInt(12) + 1;
+		int each = 425/(r+1);
+		int c = each;
+		for(int i=0; i<r; i++) {
+			Line line = new Line();
+			Label space = new Label("A");
+			space.setStyle("-fx-font-size: 20; -fx-font-weight: bold; -fx-font-family: ComicSansMS");
+			space.relocate(c-6.5, 70);
+			line.setStartX(c-10);
+			line.setStartY(100);
+			line.setEndX(c+11);
+			line.setEndY(100);
+			this.drawing.getChildren().add(line);
+			this.drawing.getChildren().add(space);
+			c += each;
+		}
+	}
 }
